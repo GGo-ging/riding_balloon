de0ddf7541795ce7da79736ab7929cc1a3de247e
seojw0124 <wjddn2000124@gmail.com>
2024-08-13 01:35:24 +0900

    feat: 네비게이션 기능 추가

--- app/src/main/java/com/example/riding_balloon/presentation/MainActivity.kt
+++ app/src/main/java/com/example/riding_balloon/presentation/MainActivity.kt
@@ -1,21 +1,49 @@
 package com.example.riding_balloon.presentation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.riding_balloon.R
+import com.example.riding_balloon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
+        }
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() = with(binding) {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigationMain.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_my_page -> {
+                    bottomNavigationMain.visibility = View.VISIBLE
+                    layoutToolbarMain.root.visibility = View.VISIBLE
+                }
+                else -> {
+                    bottomNavigationMain.visibility = View.GONE
+                    layoutToolbarMain.root.visibility = View.GONE
+                }
+            }
         }
     }
 }
