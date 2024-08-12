0a5f275b8f05076944ff5ccdba415d71be1407ae
seojw0124 <wjddn2000124@gmail.com>
2024-08-13 00:39:01 +0900

    refactor: data, presentation 폴더로 분리

--- app/src/main/java/com/example/riding_balloon/presentation/MainActivity.kt
+++ app/src/main/java/com/example/riding_balloon/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.riding_balloon
+package com.example.riding_balloon.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.riding_balloon.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
