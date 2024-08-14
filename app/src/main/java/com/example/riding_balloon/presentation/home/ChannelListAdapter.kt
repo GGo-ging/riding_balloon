a61df66a000963ab88e9e8d505a018483581678c
kkevi <bora_kim980@naver.com>
2024-08-14 21:37:22 +0900

    chore : 데이터 바인딩 중...

--- app/src/main/java/com/example/riding_balloon/presentation/home/ChannelListAdapter.kt
+++ app/src/main/java/com/example/riding_balloon/presentation/home/ChannelListAdapter.kt
@@ -1,25 +1,57 @@
 package com.example.riding_balloon.presentation.home
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.riding_balloon.databinding.ItemGridChannelListBinding
+import com.example.riding_balloon.databinding.ItemGridVideoBinding
 import com.example.riding_balloon.presentation.model.ChannelListModel
+import com.example.riding_balloon.presentation.model.FavoriteVideoInfo
+import com.example.riding_balloon.presentation.mypage.FavoriteVideoListAdapter.FavoriteVideoHolder
 
-class ChannelListAdapter: ListAdapter<ChannelListModel, ChannelListAdapter.Holder>(ChannelDiffCallback()) {
-    class Holder(private val binding: ItemGridChannelListBinding) : RecyclerView.ViewHolder(binding.root) {
+class ChannelListAdapter(): ListAdapter<ChannelListModel, ChannelListAdapter.ChannelListHolder>(ChannelDiffCallback()) {
+    interface ItemClick {
+        fun onClickItem(position: Int, item : ChannelListModel)
+    }
+    var itemClick: ItemClick? = null
+
+    class ChannelListHolder(private val binding: ItemGridChannelListBinding) : RecyclerView.ViewHolder(binding.root) {
         val image = binding.ivChannelImage
         val channelName = binding.tvChannelName
         val subscribers = binding.tvChannelSubscribers
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChannelListHolder {
+        val binding = ItemGridChannelListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ChannelListHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ChannelListHolder, position: Int) {
+        val item = getItem(position)
+
+        with(holder){
+            itemView.setOnClickListener {
+                itemClick?.onClickItem(position, item)
+                notifyItemChanged(position)
+            }
+
+            channelName.text = item.name
+            subscribers.text = item.subscribers
+        }
+    }
+
+    companion object {
+        fun from(parent: ViewGroup, onClick: (FavoriteVideoInfo) -> Unit): ChannelListHolder {
+            return ChannelListHolder(
+                ItemGridChannelListBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
     }
 }
 
--- app/src/main/java/com/example/riding_balloon/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/riding_balloon/presentation/home/HomeFragment.kt
@@ -8,11 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.riding_balloon.databinding.FragmentHomeBinding
+import com.example.riding_balloon.presentation.mypage.FavoriteVideoListAdapter
 
 class HomeFragment : Fragment() {
-//    private val binding: FragmentHomeBinding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val channelListAdapter by lazy { ChannelListAdapter() }
     private val homeViewModel by activityViewModels<HomeViewModel>()
 
     override fun onCreateView(
@@ -26,20 +28,20 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initView()
         initViewModel()
+        initView()
     }
 
     private fun initView() = with(binding){
+        rvChannelList.adapter = channelListAdapter
 
+        homeViewModel.channelList.observe(viewLifecycleOwner){ itemList ->
+            channelListAdapter.submitList(itemList)
+        }
     }
 
     private fun initViewModel(){
         homeViewModel.fetchChannel()
-
-        homeViewModel.channelList.observe(viewLifecycleOwner){ itemList ->
-            Log.d("뭐여 왜 로그 안 찍힘;", homeViewModel.channelList.value.toString())
-        }
     }
 
     override fun onDestroyView() {
