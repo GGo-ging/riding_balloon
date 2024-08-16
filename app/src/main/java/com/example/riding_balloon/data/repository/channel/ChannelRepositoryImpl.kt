4b23ff4e96bbf8deedcf12f67a03f84ad053ea7f
Jisung15 <gimseongjin302@gmail.com>
2024-08-16 20:33:01 +0900

    chore : libs파일 agp 버전 수정, build gradle 수정

--- app/src/main/java/com/example/riding_balloon/data/repository/channel/ChannelRepositoryImpl.kt
+++ app/src/main/java/com/example/riding_balloon/data/repository/channel/ChannelRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package com.example.riding_balloon.data.repository.channel
 
 import com.example.riding_balloon.data.model.channel.ChannelDetailsResponse
-import com.jeongu.imagesearchapp.network.RetrofitClient.youtubeApi
+import com.example.riding_balloon.network.RetrofitClient.youtubeApi
 
 class ChannelRepositoryImpl: ChannelRepository {
     override suspend fun getChannel(id: String): ChannelDetailsResponse {
