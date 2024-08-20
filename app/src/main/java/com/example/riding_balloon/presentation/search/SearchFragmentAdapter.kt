e7c72a502ea84b23cc21716b75fa6d66a8a015db
kkevi <bora_kim980@naver.com>
2024-08-20 14:10:23 +0900

    chore : itemClick 리스너 추가

--- app/src/main/java/com/example/riding_balloon/presentation/search/SearchFragmentAdapter.kt
+++ app/src/main/java/com/example/riding_balloon/presentation/search/SearchFragmentAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.riding_balloon.presentation.search
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,20 +11,24 @@ import com.bumptech.glide.Glide
 import com.example.riding_balloon.R
 import com.example.riding_balloon.data.model.TravelSpotInfo
 
-class SearchFragmentAdapter(private var data : List<TravelSpotInfo>
+class SearchFragmentAdapter(
+    private var data: List<TravelSpotInfo>,
 ) : RecyclerView.Adapter<SearchFragmentAdapter.ViewHolder>() {
-        interface ItemClick {
-            fun onClick(view: View, position: Int)
-        }
+    var itemClick : ItemClick? = null
+
+    interface ItemClick {
+        fun onClickItem(view : View, position: Int)
+    }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView : ImageView = itemView.findViewById(R.id.iv_item_image)
-        val countryTextView : TextView = itemView.findViewById(R.id.it_country_name)
-        val cityTextView : TextView = itemView.findViewById(R.id.it_city_name)
+        val imageView: ImageView = itemView.findViewById(R.id.iv_item_image)
+        val countryTextView: TextView = itemView.findViewById(R.id.it_country_name)
+        val cityTextView: TextView = itemView.findViewById(R.id.it_city_name)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_gridview_list, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_gridview_list, parent, false)
 
         return ViewHolder(view)
     }
@@ -32,6 +37,11 @@ class SearchFragmentAdapter(private var data : List<TravelSpotInfo>
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val data = data[position]
+
+        holder.itemView.setOnClickListener{
+            itemClick?.onClickItem(it, position)
+        }
+
         Glide.with(holder.imageView.context)
             .load(data.thumbnailUrl)
             .into(holder.imageView)
